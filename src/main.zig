eb7ca88c29307b769720d8d5d88e79b635aae7bd
Pranav AVN <pranavavn@outlook.com>
2024-11-30 09:39:06 +0000

    HTTP Server

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,49 @@
 const std = @import("std");
+const net = std.net;
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+pub fn handleConnection(conn: net.Server.Connection, stdout: std.fs.File.Writer) !void {
+    defer conn.stream.close();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    var buf: [300]u8 = undefined;
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    const bytes = try conn.stream.read(&buf);
+    try stdout.print("[INFO] Received {d} bytes from client - {s}\n", .{ bytes, buf[0..bytes] });
 
-    try bw.flush(); // don't forget to flush!
+    _ = try conn.stream.write(
+        \\HTTP/1.1 200 OK
+        \\Content-Type: text/html; charset=UTF-8
+        \\Content-Length: 2000
+        \\
+        \\<!DOCTYPE html>
+        \\<html>
+        \\    <head>
+        \\        <title>Web server</title>
+        \\    </head>
+        \\    <body>
+        \\        <h1>My Zig web server</h1>
+        \\        <p>hello world!</p>
+        \\    </body>
+        \\</html>
+    );
+    // _ = try conn.stream.write("Hello from server!");
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+pub fn main() !void {
+    const stdout = std.io.getStdOut().writer();
+
+    const address = try net.Address.resolveIp("0.0.0.0", 3000);
+    var server = try address.listen(.{
+        .reuse_port = true,
+        .reuse_address = true,
+    });
+    defer server.deinit();
+
+    try stdout.print("[INFO] Server listening on {}\n", .{server.listen_address});
+
+    while (true) {
+        const conn = try server.accept();
+        errdefer conn.stream.close(); // If the thread fails to be created
+
+        _ = try std.Thread.spawn(.{}, handleConnection, .{ conn, stdout });
+    }
 }
